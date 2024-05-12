2c93741f6eee85d1384fcd84a32f5d308c5b299c
John DeSilva <desilvjo@umich.edu>
2024-05-12 18:14:03 -0400

    Rearrange

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -1,7 +1,48 @@
 # frozen_string_literal: true
 
-require "gem_changes/gem"
-require "gem_changes/gemfile"
-require "gem_changes/change"
-require "gem_changes/plugin"
-require "gem_changes/summary_table"
+require "gem_changes"
+
+module Danger
+  # This is your plugin class. Any attributes or methods you expose here will
+  # be available from within your Dangerfile.
+  #
+  # To be published on the Danger plugins site, you will need to have
+  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
+  # for generating documentation from your plugin source, and you can verify
+  # by running `danger plugins lint` or `bundle exec rake spec`.
+  #
+  # You should replace these comments with a public description of your library.
+  #
+  # @example Ensure people are well warned about merging on Mondays
+  #
+  #          my_plugin.warn_on_mondays
+  #
+  # @see  Aesthetikx/danger-gems
+  # @tags monday, weekends, time, rattata
+  #
+  class DangerGemChanges < Plugin
+    def summarize_changes
+      markdown GemChanges::SummaryTable.new(changes: changes).markdown
+    end
+
+    def changes
+      GemChanges::Gemfile.changes(git: git)
+    end
+
+    def additions
+      changes.select(&:addition?)
+    end
+
+    def removals
+      changes.select(&:removal?)
+    end
+
+    def upgrades
+      changes.select(&:upgrade?)
+    end
+
+    def downgrades
+      changes.select(&:downgrade?)
+    end
+  end
+end
--- lib/gem_changes.rb
+++ lib/gem_changes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require "gem_changes/version"
+require "gem_changes/change"
 require "gem_changes/gem"
 require "gem_changes/gemfile"
-require "gem_changes/change"
 require "gem_changes/summary_table"
+require "gem_changes/version"
