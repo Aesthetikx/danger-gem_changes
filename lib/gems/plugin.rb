57f3c9f850720478509dddaf9ce5aa157696f97a
John DeSilva <desilvjo@umich.edu>
2024-05-12 11:40:16 -0400

    Generate markdown table

--- lib/gems/plugin.rb
+++ lib/gems/plugin.rb
@@ -25,7 +25,36 @@ module Danger
     REMOVAL_REGEX = /^-    ([^ ]*) \((.*)\)/.freeze
     ADDITION_REGEX = /^\+    ([^ ]*) \((.*)\)/.freeze
 
-    Gem = Struct.new(:name, keyword_init: true)
+    Gem = Struct.new(:name, keyword_init: true) do
+      def rubygems_uri
+        "https://rubygems.org/gems/#{name}"
+      end
+
+      def changelog_uri
+        rubygems_document.css("a#changelog").first["href"]
+      rescue StandardError
+        nil
+      end
+
+      def source_code_uri
+        code = rubygems_document.at_css("a#code")
+        home = rubygems_document.at_css("a#home")
+
+        code&.[]("href") || home&.[]("href")
+      rescue StandardError
+        nil
+      end
+
+      private
+
+      def rubygems_document
+        @rubygems_document ||= Nokogiri::HTML(rubygems_html)
+      end
+
+      def rubygems_html
+        OpenURI.open_uri(rubygems_uri).read
+      end
+    end
 
     Change = Struct.new(:gem, :from, :to, keyword_init: true) do
       def initialize(gem:, from:, to:)
@@ -61,6 +90,86 @@ module Danger
         else ::Gem::Version.new(something)
         end
       end
+    end
+
+    class ChangeTableEntry
+      attr_reader :gem, :change
+
+      def initialize(change:)
+        @change = change
+        @gem = change.gem
+      end
+
+      def rubygems_link
+        "[#{gem.name}](#{gem.rubygems_uri})"
+      end
+
+      def source_link
+        "[Source](#{gem.source_code_uri})"
+      end
+
+      def changelog_link
+        "[Changelog](#{gem.changelog_uri})"
+      end
+
+      def change_link
+        text = change_text
+        link = compare_uri
+
+        if link
+          "[#{text}](#{link})"
+        else
+          text
+        end
+      end
+
+      private
+
+      def change_text
+        if change.addition?
+          "Added at #{change.to}"
+        elsif change.removal?
+          "Removed at #{change.from}"
+        elsif change.upgrade?
+          "v#{change.from} -> v#{change.to}"
+        elsif change.downgrade?
+          "v#{change.to} <- v#{change.from}"
+        else
+          fail "Unknown change type"
+        end
+      end
+
+      def compare_uri
+        return nil unless change.change?
+
+        from, to = [change.from, change.to].sort
+
+        "#{gem.source_code_uri}/compare/v#{from}...v#{to}"
+      end
+    end
+
+    def summarize_changes
+      message = "### Gemfile.lock Changes\n"
+
+      message += "| Gem | Source | Changelog | Change |\n"
+      message += "| --- | ------ | --------- | ------ |\n"
+
+      changes.each do |change|
+        message += "| "
+
+        entry = ChangeTableEntry.new(change: change)
+
+        message += [
+          entry.rubygems_link,
+          entry.source_link,
+          entry.changelog_link,
+          entry.change_link
+        ].join(" | ")
+
+        message += " |\n"
+      end
+
+      markdown message
     end
 
     def changes
--- spec/gems_spec.rb
+++ spec/gems_spec.rb
@@ -86,6 +86,22 @@ module Danger
           ]
         )
       end
+
+      it "can generate a markdown table" do
+        @my_plugin.summarize_changes
+
+        markdown = @dangerfile.status_report[:markdowns].last.message
+
+        expect(markdown).to eq(<<~MARKDOWN)
+          ### Gemfile.lock Changes
+          | Gem | Source | Changelog | Change |
+          | --- | ------ | --------- | ------ |
+          | [rubocop-factory_bot](https://rubygems.org/gems/rubocop-factory_bot) | [Source](https://github.com/rubocop/rubocop-factory_bot) | [Changelog](https://github.com/rubocop/rubocop-factory_bot/blob/master/CHANGELOG.md) | [v2.25.0 <- v2.25.1](https://github.com/rubocop/rubocop-factory_bot/compare/v2.25.0...v2.25.1) |
+          | [rubocop-performance](https://rubygems.org/gems/rubocop-performance) | [Source](https://github.com/rubocop/rubocop-performance/) | [Changelog](https://github.com/rubocop/rubocop-performance/blob/master/CHANGELOG.md) | Added at 1.21.0 |
+          | [rubocop-rake](https://rubygems.org/gems/rubocop-rake) | [Source](https://github.com/rubocop/rubocop-rake) | [Changelog](https://github.com/rubocop/rubocop-rake/blob/master/CHANGELOG.md) | [v0.6.0 -> v0.6.1](https://github.com/rubocop/rubocop-rake/compare/v0.6.0...v0.6.1) |
+          | [rubocop-rspec](https://rubygems.org/gems/rubocop-rspec) | [Source](https://github.com/rubocop/rubocop-rspec) | [Changelog](https://github.com/rubocop/rubocop-rspec/blob/master/CHANGELOG.md) | Removed at 2.29.2 |
+        MARKDOWN
+      end
     end
   end
 end
