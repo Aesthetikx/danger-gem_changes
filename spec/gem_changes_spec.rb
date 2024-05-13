d47e32678c2a2517bf46e4b6a85a91f06ae04348
John DeSilva <desilvjo@umich.edu>
2024-05-12 21:33:11 -0400

    Move markdown to fixture

--- spec/gem_changes_spec.rb
+++ spec/gem_changes_spec.rb
@@ -90,17 +90,11 @@ module Danger
       it "can generate a markdown table" do
         @my_plugin.summarize_changes
 
-        markdown = @dangerfile.status_report[:markdowns].last.message
+        actual = @dangerfile.status_report[:markdowns].last.message
 
-        expect(markdown).to eq(<<~MARKDOWN)
-          ### Gemfile.lock Changes
-          | Gem | Source | Changelog | Change | Level |
-          | --- | ------ | --------- | ------ | ----- |
-          | [rubocop-factory_bot](https://rubygems.org/gems/rubocop-factory_bot) | [Source](https://github.com/rubocop/rubocop-factory_bot) | [Changelog](https://github.com/rubocop/rubocop-factory_bot/blob/master/CHANGELOG.md) | [v2.25.0 <- v2.25.1](https://github.com/rubocop/rubocop-factory_bot/compare/v2.25.0...v2.25.1) | Patch |
-          | [rubocop-performance](https://rubygems.org/gems/rubocop-performance) | [Source](https://github.com/rubocop/rubocop-performance) | [Changelog](https://github.com/rubocop/rubocop-performance/blob/master/CHANGELOG.md) | Added at 1.21.0 |  |
-          | [rubocop-rake](https://rubygems.org/gems/rubocop-rake) | [Source](https://github.com/rubocop/rubocop-rake) | [Changelog](https://github.com/rubocop/rubocop-rake/blob/master/CHANGELOG.md) | [v0.5.0 -> v0.6.0](https://github.com/rubocop/rubocop-rake/compare/v0.5.0...v0.6.0) | Minor |
-          | [rubocop-rspec](https://rubygems.org/gems/rubocop-rspec) | [Source](https://github.com/rubocop/rubocop-rspec) | [Changelog](https://github.com/rubocop/rubocop-rspec/blob/master/CHANGELOG.md) | Removed at 2.29.2 |  |
-        MARKDOWN
+        expected = File.read("spec/fixtures/summary_table.markdown")
+
+        expect(actual).to eq(expected)
       end
     end
   end
