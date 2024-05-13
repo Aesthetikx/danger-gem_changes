eaabf27be3cdb5718e580e0498ef95ed8af5be1f
John DeSilva <desilvjo@umich.edu>
2024-05-13 17:20:32 -0400

    Document public interface
    Closes #4.

--- lib/danger_plugin.rb
+++ lib/danger_plugin.rb
@@ -3,24 +3,18 @@
 require "gem_changes"
 
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
+  # A Danger plugin that can help code review for PRs that have changes to Gemfile.lock.
   #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
+  # @example Print a markdown table summarizing all gem dependency changes
   #
-  # You should replace these comments with a public description of your library.
+  #          gem_changes.summarize_changes
   #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  Aesthetikx/danger-gems
-  # @tags monday, weekends, time, rattata
-  #
+  # @see Aesthetikx/danger-gem_changes
   class DangerGemChanges < Plugin
+    # Print a summary of the changes to the Gemfile.lock.
+    # @param changes [Array<GemChanges::Change>] An optional list of changes to summarize, defaulting to all changes.
+    # @param title [String] An optional title for the header.
+    # @return [void]
     def summarize_changes(changes: self.changes, title: "Gemfile.lock Changes")
       return if changes.empty?
 
@@ -31,22 +25,32 @@ module Danger
       markdown string
     end
 
+    # All dependencies that have changed.
+    # @return [Array<GemChanges::Change>]
     def changes
       GemChanges::Gemfile.changes(git:)
     end
 
+    # New dependencies that have been added.
+    # @return [Array<GemChanges::Change>]
     def additions
       changes.select(&:addition?)
     end
 
+    # Dependencies that have been removed.
+    # @return [Array<GemChanges::Change>]
     def removals
       changes.select(&:removal?)
     end
 
+    # Dependencies that were upgraded.
+    # @return [Array<GemChanges::Change>]
     def upgrades
       changes.select(&:upgrade?)
     end
 
+    # Dependencies that were downgraded.
+    # @return [Array<GemChanges::Change>]
     def downgrades
       changes.select(&:downgrade?)
     end
